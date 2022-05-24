5ba0238336bc2ed8827200e7667bc1cea9d088e2
Alexandra Harley <aharleydev@gmail.com>
2022-05-23 22:53:49 -0400

    skeleton layout with notes to continue. site only, mobile later

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// code so background changes on refresh
+
+// create an array with the different image srcs
+
+// declare random number variable: math.floor(math.random() * 6)
+
+// input array[r] for source
+
+// document queryselector html style background url
